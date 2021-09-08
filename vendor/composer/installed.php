9cf8b0ee8d7b98fabd24ea6c80c904d15669338b
root <root@DESKTOP-6I1A19J.localdomain>
2021-09-08 12:48:19 +0100

    Commit config and vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'woocommerce-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'p3/woocommerce',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'p3/php-sdk' => array(
+            'pretty_version' => '1.3.4',
+            'version' => '1.3.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../p3/php-sdk',
+            'aliases' => array(),
+            'reference' => '8831790e8a273694ce3a7a08eec6211466ab57a5',
+            'dev_requirement' => false,
+        ),
+        'p3/woocommerce' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'woocommerce-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
